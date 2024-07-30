f8364ffa3fdce766bd91be3fac91a48cc64d3d0b
ABC <ABC@DESKTOP-NAUR8NN>
2024-07-30 14:00:04 +0530

    hello adi

--- tanmay_bhai/src/tanmay_bhai69/Main.java
+++ tanmay_bhai/src/tanmay_bhai69/Main.java
@@ -3,6 +3,8 @@ package tanmay_bhai69;
 public class Main {
 public static void main(String[] args) {
 	System.out.println("Hello World");
+	System.out.println("Hello Tanmay");
+	System.out.println("Hello Adi");
 	
 }
 }
